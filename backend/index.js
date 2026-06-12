3e08f4b23cccf49d4efe172311078d52817aad1b
Shivanipatil-4 <shivanishivapatil@gmail.com>
2026-06-12 21:03:06 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://ai-powered-ecommerce-website-t69c.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
